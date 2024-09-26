b2514411695ef61262fb7c39985efbf143fcb1cf
Scott Williams <scottwill1999@gmail.com>
2024-09-25 23:27:17 -0400

    setup script

--- scripts/createTable.sql
+++ scripts/createTable.sql
@@ -1,3 +1,19 @@
+
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'ExpenseTracker') 
+BEGIN
+CREATE DATABASE [ExpenseTracker]
+END
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Categories' AND xtype='U')
+BEGIN
+Create TABLE Categories(
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    Type NVARCHAR(255) Not NULL
+)
+END
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Expenses' AND xtype='U')
+BEGIN
 CREATE TABLE Expenses (
     Id INT PRIMARY KEY IDENTITY(1,1),
     Description NVARCHAR(255) NOT NULL,
@@ -5,8 +21,4 @@ CREATE TABLE Expenses (
     Amount DECIMAL(10, 2) NOT NULL,
     Date DATETIME DEFAULT GETDATE()
 );
-
-Create TABLE Categories(
-    Id INT PRIMARY KEY IDENTITY(1,1),
-    Type NVARCHAR(255) Not NULL
-)
+END
