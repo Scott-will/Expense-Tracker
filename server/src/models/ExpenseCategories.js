6f506563fb0428e1143823230eeadccb0e740822
Scott Williams <scottwill1999@gmail.com>
2024-10-04 20:41:00 -0400

    switch to typescript

--- server/src/models/ExpenseCategories.js
+++ server/src/models/ExpenseCategories.js
@@ -3,23 +3,27 @@ const { sql } = require('../config/db');
 class ExpenseCategories{
 
     static async getCategories() {
-        const result = await sql.query`SELECT * FROM ${process.env.DB_NAME}.dbo.ExpenseCategories`;
+        const query = `SELECT * FROM ${process.env.DB_NAME}.dbo.ExpenseCategories`;
+        const result = await sql.query(query);
         return result.recordset;
     }    
 
     static async getCategoryById(id){
-        const result = await sql.query`SELECT * FROM ${process.env.DB_NAME}.dbo.ExpenseCategories Where Id = ${id}`
+        const query = `SELECT * FROM ${process.env.DB_NAME}.dbo.ExpenseCategories Where Id = ${id}`
+        const result = await sql.query(query);
         return result.recordset
     }
 
     static async getCategoryByCategory(category){
-        const result = await sql.query`SELECT * FROM ${process.env.DB_NAME}.dbo.ExpenseCategories Where Category = ${category}`
+        const query = `SELECT * FROM ${process.env.DB_NAME}.dbo.ExpenseCategories Where Category = '${category}'`
+            const result = await sql.query(query);
         return result.recordset
     }
 
     static async addExpenseCategory(category) {
         try{
-            const result = await sql.query`INSERT INTO ${process.env.DB_NAME}.dbo.ExpenseCategories (category) VALUES (${category})`;
+            const query = `INSERT INTO ${process.env.DB_NAME}.dbo.ExpenseCategories (category) VALUES ('${category}')`
+            const result = await sql.query(query);
             return result.rowsAffected;
         }
         catch(error){
@@ -30,7 +34,8 @@ class ExpenseCategories{
 
     static async updateExpenseCategory(categoryObj) {
         try{
-            const result = await sql.query`Update ${process.env.DB_NAME}.dbo.ExpenseCategories SET category = (${categoryObj.category}) Where Id = ${categoryObj.id}`;
+            const query = `Update ${process.env.DB_NAME}.dbo.ExpenseCategories SET category = '(${categoryObj.category})' Where Id = ${categoryObj.id}`;
+            const result = await sql.query(query);
             return result.rowsAffected;
         }
         catch(error){
@@ -40,12 +45,14 @@ class ExpenseCategories{
     }
 
     static async deleteCategoryById(id) {
-        const result = await sql.query`DELETE FROM ${process.env.DB_NAME}.dbo.ExpenseCategories Where Id = ${id}`;
+        const query = `DELETE FROM ${process.env.DB_NAME}.dbo.ExpenseCategories Where Id = ${id}`;
+        const result = await sql.query(query);
         return result.recordset;
     } 
     
     static async deleteCategoryByCategory(category) {
-        const result = await sql.query`DELETE FROM ${process.env.DB_NAME}.dbo.ExpenseCategories Where Category = ${category}`;
+        const query = `DELETE FROM ${process.env.DB_NAME}.dbo.ExpenseCategories Where Category = '${category}'`;
+        const result = await sql.query(query);
         return result.recordset;
     } 
 }
